60c18a8e504701e969e287b7ad8c940102d364b7
Viacheslav Semenkov <ssemenkoff@gmail.com>
2022-06-09 18:33:57 +0000

    Fixed public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/scheme-editor/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/schema_editor/' : '/',
   transpileDependencies: [
     'vuetify'
   ],
