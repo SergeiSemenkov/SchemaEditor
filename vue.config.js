c3df3101ddf500e524236e22b7f6234337d16781
Viacheslav Semenkov <ssemenkoff@gmail.com>
2022-06-09 18:55:50 +0000

    Fix public path 2

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/schema_editor/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/emondrian/schema_editor/' : '/',
   transpileDependencies: [
     'vuetify'
   ],
